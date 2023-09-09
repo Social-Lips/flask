bb6d9fd4866524f54ab2665149626c23ad93aec6
sashika20643 <sashikadulaj20643@gmail.com>
2023-09-10 04:50:53 +0530

    connected with mongo

--- flask+firebase/server.py
+++ flask+firebase/server.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, send_file,render_template
+from flask import Flask, request,render_template, jsonify
 import os
 import firebase_admin
 from firebase_admin import credentials, storage
+from pymongo import MongoClient
+from bson import ObjectId
+
 
 cred = credentials.Certificate("./social-lips-firebase-adminsdk-c28zn-d607a10c33.json")
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://ishan:1998@cluster0.zhsvvlw.mongodb.net/?retryWrites=true&w=majority'
+mongo = MongoClient(app.config['MONGO_URI'])
+db = mongo.test
 
 # Function to download a video from Firebase Storage
 def download_video(file_id, save_directory):
@@ -20,9 +26,9 @@ def download_video(file_id, save_directory):
     return file_path
 
 # Function to create a VTT subtitle file
-def create_subtitle_file(video_file_path):
+def create_subtitle_file(file_name):
     subtitle_text = "WEBVTT\n\n0:00:00.000 --> 0:00:02.000\nSubtitle line 1\n\n0:00:02.001 --> 0:00:04.000\nSubtitle line 2"
-    file_name = os.path.splitext(os.path.basename(video_file_path))[0]
+  
     subtitle_file_path = "subtitle/" + file_name + ".vtt"
     with open(subtitle_file_path, "w", encoding="utf-8") as vtt_file:
         vtt_file.write(subtitle_text)
@@ -48,13 +54,13 @@ def download_file(file_id):
     try:
         save_directory = "videos/"
         video_file_path = download_video(file_id, save_directory)
-        subtitle_file_path = create_subtitle_file(video_file_path)
-        destination_blob_name = f"posts/subtitles/{os.path.basename(video_file_path)}.vtt"
+        subtitle_file_path = create_subtitle_file(file_id)
+        destination_blob_name = f"posts/subtitles/{file_id}.vtt"
         upload_subtitle_to_firebase(subtitle_file_path, destination_blob_name)
         
         # Clean up: Remove the local VTT and video files
-        # os.remove(subtitle_file_path)
-        # os.remove(video_file_path)
+        os.remove(subtitle_file_path)
+        os.remove(video_file_path)
 
         return f"Subtitles uploaded to Firebase Storage as {destination_blob_name}"
     except Exception as e:
@@ -83,6 +89,17 @@ def upload_file():
         return "File uploaded successfully"
     except Exception as e:
         return str(e), 500
-
+@app.route('/posts/<string:post_id>', methods=['GET'])
+def get_post(post_id):
+    try:
+        collection = db['posts']  # Replace 'posts' with your collection name
+        post = collection.find_one({"_id": ObjectId(post_id)})
+        if post:
+            del post['_id']
+            return jsonify({"post": post})
+        else:
+            return jsonify({"message": "Post not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
     app.run(debug=True)
