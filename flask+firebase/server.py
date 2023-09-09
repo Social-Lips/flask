a47894d0230fbd021b9ef3bb89b902bf8013eaa6
sashika20643 <sashikadulaj20643@gmail.com>
2023-09-10 04:22:55 +0530

    code restructured

--- flask+firebase/server.py
+++ flask+firebase/server.py
@@ -8,51 +8,58 @@ firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
+# Function to download a video from Firebase Storage
+def download_video(file_id, save_directory):
+    bucket = firebase_admin.storage.bucket(app=firebase_admin.get_app(), name="social-lips.appspot.com")
+    firebase_blob_path = "posts/video/" + file_id
+    blob = bucket.blob(firebase_blob_path)
+    file_name = os.path.basename(file_id)
+    file_path = os.path.join(save_directory, file_name + ".mp4")
+    blob.download_to_filename(file_path)
+    print("file downloaded")
+    return file_path
+
+# Function to create a VTT subtitle file
+def create_subtitle_file(video_file_path):
+    subtitle_text = "WEBVTT\n\n0:00:00.000 --> 0:00:02.000\nSubtitle line 1\n\n0:00:02.001 --> 0:00:04.000\nSubtitle line 2"
+    file_name = os.path.splitext(os.path.basename(video_file_path))[0]
+    subtitle_file_path = "subtitle/" + file_name + ".vtt"
+    with open(subtitle_file_path, "w", encoding="utf-8") as vtt_file:
+        vtt_file.write(subtitle_text)
+    print("Subtitle generated")
+    return subtitle_file_path
+
+# Function to upload a subtitle file to Firebase Storage
+def upload_subtitle_to_firebase(subtitle_file_path, destination_blob_name):
+    bucket = firebase_admin.storage.bucket(app=firebase_admin.get_app(), name="social-lips.appspot.com")
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(subtitle_file_path)
+    print("Subtitle uploaded")
+    return 1;
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/download/<string:file_id>', methods=['GET'])
 def download_file(file_id):
-    bucket = firebase_admin.storage.bucket(app=firebase_admin.get_app(), name="social-lips.appspot.com")
 
     try:
-        firebase_blob_path = "posts/video/" + file_id  
-        
-        # Get a reference to the specified file in Firebase Storage
-        blob = bucket.blob(firebase_blob_path)
-        
-      
-        file_name = os.path.basename(file_id)  # Extract  filename from the path
-      
-  
         save_directory = "videos/"
-        file_path = os.path.join(save_directory, file_name+".mp4")
-        # return file_path
-        # Download the file from Firebase Storage to the specified path
-        blob.download_to_filename(file_path)
+        video_file_path = download_video(file_id, save_directory)
+        subtitle_file_path = create_subtitle_file(video_file_path)
+        destination_blob_name = f"posts/subtitles/{os.path.basename(video_file_path)}.vtt"
+        upload_subtitle_to_firebase(subtitle_file_path, destination_blob_name)
+        
+        # Clean up: Remove the local VTT and video files
+        # os.remove(subtitle_file_path)
+        # os.remove(video_file_path)
 
-        subtitle_text = "WEBVTT\n\n0:00:00.000 --> 0:00:02.000\nSubtitle line 1\n\n0:00:02.001 --> 0:00:04.000\nSubtitle line 2"
-        subtitle_file_path="subtitle/"+file_name+".vtt"
-        with open(subtitle_file_path, "w", encoding="utf-8") as vtt_file:
-            vtt_file.write(subtitle_text)
-
-
-
-    
-
-    # Upload the VTT subtitle file to the "posts/subtitles" folder in Firebase Storage
-        destination_blob_name = f"posts/subtitles/{file_name}.vtt"
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(subtitle_file_path)
-
-        # Clean up: Remove the local VTT file
-        os.remove(subtitle_file_path)
-        os.remove(file_path)
-        # Send a response to indicate the file has been downloaded
-        return f"File uploaded  {subtitle_file_path}"
+        return f"Subtitles uploaded to Firebase Storage as {destination_blob_name}"
     except Exception as e:
         return str(e), 404
+    
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
